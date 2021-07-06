0c98ec462e1e178d8360c75374be4945dbffe3e3
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2021-07-05 23:20:33 -0300

    primeiros passos com o flutter

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Texto dentro de outro column',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
